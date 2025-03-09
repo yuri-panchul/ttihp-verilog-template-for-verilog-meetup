a4c493a417b7ebc1c1aa1f0b106385a2c434edfc
Uri Shaked <uri@urishaked.com>
2025-03-09 13:05:21 +0200

    chore: adapt for IHP shuttle

--- test/tb.v
+++ test/tb.v
@@ -22,10 +22,6 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
 
   // Replace tt_um_example with your module name:
   tt_um_example user_project (
