1bab8a8cb77629cbb42f0eac4a86553991a0615c
Yuri Panchul <yuri@panchul.com>
2025-10-18 15:23:37 -0700

    Moved some info from tt10 to ttihp verilog meetup template

--- src/project.v
+++ src/project.v
@@ -1,27 +1,82 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Tiny Tapeout and Verilog Meetup
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_verilog_meetup_template_project_TODO
+(
+    input  [7:0] ui_in,    // Dedicated inputs
+    output [7:0] uo_out,   // Dedicated outputs
+    input  [7:0] uio_in,   // IOs: Input path
+    output [7:0] uio_out,  // IOs: Output path
+    output [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input        ena,      // always 1 when the design is powered, so you can ignore it
+    input        clk,      // clock
+    input        rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    //------------------------------------------------------------------------
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire       tm1638_clk;
+    wire       tm1638_stb;
+
+    wire       tm1638_dio_in;
+    wire       tm1638_dio_out;
+    wire       tm1638_dio_out_en;
+
+    wire       vga_hsync;
+    wire       vga_vsync;
+
+    wire [1:0] vga_red;
+    wire [1:0] vga_green;
+    wire [1:0] vga_blue;
+
+    wire       sticky_failure;
+
+    //------------------------------------------------------------------------
+
+    layer_between_project_and_lab_top i_layer
+    (
+        .clock (   clk   ),
+        .reset ( ~ rst_n ),
+        .*
+    );
+
+    //------------------------------------------------------------------------
+
+    // All output pins must be assigned. If not used, assign to 0.
+
+    assign uio_out [7]    = tm1638_stb;
+    assign uio_out [6]    = tm1638_clk;
+
+    assign uio_oe  [7:6]  = '1;
+
+    assign tm1638_dio_in  = uio_in [5];
+    assign uio_out [5]    = tm1638_dio_out;
+    assign uio_oe  [5]    = tm1638_dio_out_en;
+
+    assign uio_out [4]    = sticky_failure;
+    assign uio_oe  [4]    = '1;
+
+    assign uio_out [3:0]  = '0;
+    assign uio_oe  [3:0]  = '0;
+
+    //------------------------------------------------------------------------
+
+    assign uo_out  [0]    = vga_red   [1];
+    assign uo_out  [1]    = vga_green [1];
+    assign uo_out  [2]    = vga_blue  [1];
+    assign uo_out  [3]    = vga_vsync    ;
+    assign uo_out  [4]    = vga_red   [0];
+    assign uo_out  [5]    = vga_green [0];
+    assign uo_out  [6]    = vga_blue  [0];
+    assign uo_out  [7]    = vga_hsync    ;
+
+    //------------------------------------------------------------------------
+
+    // List all unused inputs to prevent warnings
+    wire _unused = & { ena, ui_in, uio_in [7:6], uio_in [4:0], 1'b0 };
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout and Verilog Meetup
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
@@ -25,16 +25,8 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # Wait for a number of clock cycles
+    await ClockCycles(dut.clk, 1000)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # Check there is no sticky failure so far
+    assert not ((dut.uio_out.value >> 4) & 1);
